d1cbfe91a6d4dd32fa2b7ca629dd9d40d7df7354
Devp-sriram <sriramraman100@gmail.com>
2024-11-21 10:48:01 +0530

    completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import { useRef , useState } from "react"
 
 interface Query{
-   "inputs" : HTMLInputElement
-}
-
+   "inputs" : string,
+};
 
 function App() {
- const inputRef = useRef(null)
- const [img,setImg] = useState('')
+ const inputRef = useRef<HTMLInputElement>(null)
+ const [img,setImg] = useState('');
+ const [isLoading,setIsLoading] = useState(false)
   
-  async function query(data :Query) {
-	  const response =await fetch(
+  async function query(data : Query) {
+	  try{
+    const response = await fetch(
 		"https://api-inference.huggingface.co/models/ZB-Tech/Text-to-Image",
 		 {
 			headers: {
-				Authorization: "Bearer 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",
+				Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
 				"Content-Type": "application/json",
 			},
 			method: "POST",
@@ -23,26 +24,54 @@ function App() {
 	  );
 	  const result = await response.blob();
 	  return result;
+    }catch(err){
+      console.log(err)
+    }
+
   }
 
-  function generate(){
+function generate(){
    const inputElement:HTMLInputElement | null = inputRef.current
-    if (!inputElement) {
-      console.error('Input reference is null');
-    return;
-    }
-     query({"inputs": `${inputElement.value}`}).then((response) => {
+      if (!inputElement) {
+            alert('Type anything on the Text Box');
+            return;
+      }
+   setIsLoading(true);
+     
+   query({"inputs": `${inputElement.value}`}).then((response : Blob | undefined) => {
+      if(response === undefined){
+          console.log('invalid response');
+          return
+      }
 	    const objectUrl = URL.createObjectURL(response);
+      setIsLoading(false);      
       setImg(objectUrl);
-     });
-  }
+   });
+   
+}
+
+function downloadImage(){
+   if (!img) return;
+  
+  const link = document.createElement('a');
+  link.href = img;
+  link.download = 'image.jpg';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
   return (
-    <div className="flex justify-center items-center w-screen h-screen "> 
-      <div className=" flex flex-col">
-      <label>Text to Image </label>
-      <input ref={inputRef}/>
-      <button onClick={generate}>generate</button>
-      <img src={img}/>
+    <div className="flex justify-center items-center w-screen h-screen bg-black"> 
+      <div className="w-full flex flex-col justify-center items-center gap-4">
+        <h1 className="text-2xl text-bold justify-center">Text to Image </h1>
+        <div className="flex justify-center w-full gap-2">
+          <input className="w-1/2 h-8 rounded" ref={inputRef} />
+          <button onClick={generate} className="border-solid border-2 border-white rounded-lg px-1">Generate</button>
+          <button onClick={downloadImage} className="border-solid border-2 border-white rounded-lg px-1">Download</button>
+        </div>
+        {img &&  <img src={img} className="w-1/3 h-1/3 "/> }
+        {isLoading  &&  <img src='loader.gif' className="w-1/2 h-1/2 "/> }
       </div>
     </div>
   )
